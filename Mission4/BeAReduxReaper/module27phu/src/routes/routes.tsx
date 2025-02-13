58c86d20c2b528ae0ea2303462abc992c20cbe8a
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-02-13 12:21:41 +0600

    Module 27

---
+++ Mission4/BeAReduxReaper/module27phu/src/routes/routes.tsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Contact from "../pages/Contact";
+import About from "../pages/About";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import { adminRoutes } from "./admin.routes";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: adminRoutes,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+export default router;
