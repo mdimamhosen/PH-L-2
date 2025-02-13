9beaf0fa5877d0a38901f6dd640677b3bcdebfe3
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-02-13 12:17:45 +0600

    Module 26

---
+++ Mission4/BeAReduxReaper/module26phu/src/routes/admin.routes.tsx
@@ -0,0 +1,88 @@
+import { ReactNode } from "react";
+import AdminDashboard from "../pages/Admin/AdminDashboard";
+import CreateAdmin from "../pages/Admin/CreateAdmin";
+import CreateFaculty from "../pages/Admin/CreateFaculty";
+import CreateStudent from "../pages/Admin/CreateStudent";
+import { NavLink } from "react-router-dom";
+
+export type TRoute = {
+  path: string;
+  element: ReactNode;
+};
+
+export const adminPahts = [
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+    ],
+  },
+];
+
+export const adminRoutes = adminPahts.reduce((acc: TRoute[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+export type TSideBarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TSideBarItem[];
+};
+
+export const adminSideBarItems = adminPahts.reduce(
+  (acc: TSideBarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+
+    if (item.children) {
+      acc.push({
+        key: item.name as string,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+        children: item.children.map((child) => ({
+          key: child.path,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+    return acc;
+  },
+  []
+);
