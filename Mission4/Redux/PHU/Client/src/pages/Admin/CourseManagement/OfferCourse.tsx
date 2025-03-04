34b8aa85fb32b9f54b10588f6f8abdbb42a7f970
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-03-05 01:26:54 +0600

    Next Auth

--- Mission4/Redux/PHU/Client/src/pages/Admin/CourseManagement/OfferCourse.tsx
+++ Mission4/Redux/PHU/Client/src/pages/Admin/CourseManagement/OfferCourse.tsx
@@ -1,35 +1,125 @@
 import { Button, Col, Flex } from "antd";
 import PHForm from "../../../components/Form/PHForm";
 import PHFormSelectWithWatch from "../../../components/Form/PHFormSelectWithWatch";
-import { useGetAcademicFacultiesQuery } from "../../../redux/feature/academicSemister/academicSemesterAPI";
+import {
+  useGetAcademicFacultiesQuery,
+  useGetAllAcademicDepartmentQuery,
+} from "../../../redux/feature/academicSemister/academicSemesterAPI";
 import PHFormInput from "../../../components/Form/PHFormInput";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import { useState } from "react";
+import PHFormSelect from "../../../components/Form/PHFormSelect";
+import PHTimePicker from "../../../components/Form/PHTimePicker";
+import {
+  useCreateOfferedCourseMutation,
+  useGetAllCoursesQuery,
+  useGetAllRegisteredSemestersQuery,
+  useGetCourseFacultiesQuery,
+} from "../../../redux/feature/courseManagement/courseManagement";
+import { weekDaysOptions } from "../../../Data/Global.Data";
+import moment from "moment";
 
 const OfferCourse = () => {
-  const [id, setId] = useState("");
+  const [courseId, setCourseId] = useState("");
 
+  const [addOfferedCourse] = useCreateOfferedCourseMutation();
+
+  const { data: semesterRegistrationData } = useGetAllRegisteredSemestersQuery([
+    { name: "sort", value: "year" },
+    { name: "status", value: "UPCOMING" },
+  ]);
   const { data: academicFacultyData } = useGetAcademicFacultiesQuery(undefined);
-  const academicSemesterOptions = academicFacultyData?.data?.map((item) => ({
+
+  const { data: academicDepartmentData } =
+    useGetAllAcademicDepartmentQuery(undefined);
+
+  const { data: coursesData } = useGetAllCoursesQuery(undefined);
+
+  const { data: facultiesData, isFetching: fetchingFaculties } =
+    useGetCourseFacultiesQuery(courseId, { skip: !courseId });
+
+  const semesterRegistrationOptions = semesterRegistrationData?.data?.map(
+    (item) => ({
+      value: item._id,
+      label: `${item.academicSemester.name} ${item.academicSemester.year}`,
+    })
+  );
+
+  const academicFacultyOptions = academicFacultyData?.data?.map((item) => ({
     value: item._id,
     label: item.name,
   }));
 
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+  const academicDepartmentOptions = academicDepartmentData?.data?.map(
+    (item) => ({
+      value: item._id,
+      label: item.name,
+    })
+  );
+
+  const courseOptions = coursesData?.data?.map((item) => ({
+    value: item._id,
+    label: item.title,
+  }));
+
+  const facultiesOptions = facultiesData?.data?.faculties?.map((item) => ({
+    value: item._id,
+    label: item.fullName,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const offeredCourseData = {
+      ...data,
+      maxCapacity: parseInt(data.maxCapacity),
+      section: parseInt(data.section),
+      startTime: moment(new Date(data.startTime)).format("HH:mm"),
+      endTime: moment(new Date(data.endTime)).format("HH:mm"),
+    };
+    const res = await addOfferedCourse(offeredCourseData);
+    console.log(res);
   };
 
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHFormSelectWithWatch
-            onValueChange={setId}
-            label="Academic Semester"
-            name="academicSemester"
-            options={academicSemesterOptions}
+          <PHFormSelect
+            name="semesterRegistration"
+            label="Semester Registrations"
+            options={semesterRegistrationOptions}
           />
-          <PHFormInput type="text" name="Test" label="Test" disable={!id} />
+          <PHFormSelect
+            name="academicFaculty"
+            label="Academic Faculty"
+            options={academicFacultyOptions}
+          />
+          <PHFormSelect
+            name="academicDepartment"
+            label="Academic Department"
+            options={academicDepartmentOptions}
+          />
+          <PHFormSelectWithWatch
+            onValueChange={setCourseId}
+            options={courseOptions}
+            name="course"
+            label="Course"
+          />
+          <PHFormSelect
+            disabled={!courseId || fetchingFaculties}
+            name="faculty"
+            label="Faculty"
+            options={facultiesOptions}
+          />
+          <PHFormInput type="text" name="section" label="Section" />
+          <PHFormInput type="text" name="maxCapacity" label="Max Capacity" />
+          <PHFormSelect
+            mode="multiple"
+            options={weekDaysOptions}
+            name="days"
+            label="Days"
+          />
+          <PHTimePicker name="startTime" label="Start Time" />
+          <PHTimePicker name="endTime" label="End Time" />
 
           <Button htmlType="submit">Submit</Button>
         </PHForm>
--- Mission4/Redux/PHU/Client/src/redux/feature/courseManagement/courseManagement.ts
+++ Mission4/Redux/PHU/Client/src/redux/feature/courseManagement/courseManagement.ts
@@ -83,6 +83,29 @@ const courseManagementApi = baseAPI.injectEndpoints({
       }),
       invalidatesTags: ["courses"],
     }),
+    getCourseFaculties: builder.query({
+      query: (id) => {
+        return {
+          url: `/courses/${id}/get-faculties`,
+          method: "GET",
+        };
+      },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    createOfferedCourse: builder.mutation({
+      query: (data) => ({
+        url: `offered-courses/create-offered-course`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["courses"],
+    }),
   }),
 });
 
@@ -93,4 +116,6 @@ export const {
   useGetAllCoursesQuery,
   useAddCourseMutation,
   useAddFacultiesMutation,
+  useGetCourseFacultiesQuery,
+  useCreateOfferedCourseMutation,
 } = courseManagementApi;
